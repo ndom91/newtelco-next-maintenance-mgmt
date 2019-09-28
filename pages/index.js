3d207c7ac8c697fd81795487f8af262961e60c8a
ndo@ndo3 <yo@ni.co.de>
2019-09-28 20:39:04 +0200

    add: netlify

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,73 @@
 import Layout from '../src/components/layout'
 import Link from 'next/link'
 
-const PostLink = props => (
+function getPosts () {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+  ]
+}
+
+const PostLink = ({ post }) => (
   <li>
-    <Link href='/m/[id]' as={`/m/${props.id}`}>
-      <a>{props.id}</a>
+    <Link href='/p/[id]' as={`/p/${post.id}`}>
+      <a>{post.title}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}
+    </style>
   </li>
 )
 
-const Index = () => (
-  <Layout>
-    <p>Hello Next.js</p>
-    <ul>
-      <PostLink id='maint1' />
-      <PostLink id='maint2' />
-    </ul>
-  </Layout>
-)
+export default function Blog () {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        {getPosts().map(post => (
+          <PostLink key={post.id} post={post} />
+        ))}
+      </ul>
+      <style jsx>{`
+        h1,
+        a {
+          font-family: 'Arial';
+        }
 
-export default Index
+        ul {
+          padding: 0;
+        }
+
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}
+      </style>
+    </Layout>
+  )
+}
---
+++ pages/p/[id].js
@@ -0,0 +1,35 @@
+import Layout from '../../src/components/layout';
+import { useRouter } from 'next/router';
+
+export default () => {
+  const router = useRouter();
+  console.log(router.query)
+  return (
+    <Layout>
+      <h1>{router.query.id}</h1>
+      <div className="markdown">
+        {router.query.id}
+      </div>
+      <style jsx global>{`
+        .markdown {
+          font-family: 'Arial';
+        }
+
+        .markdown a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        .markdown a:hover {
+          opacity: 0.6;
+        }
+
+        .markdown h3 {
+          margin: 0;
+          padding: 0;
+          text-transform: uppercase;
+        }
+      `}</style>
+    </Layout>
+  );
+};
