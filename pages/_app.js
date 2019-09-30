970ca132af7431675ce0d48c7ac7fb6b40661829
ndo <yo@ni.co.de>
2019-09-30 21:04:40 +0200

    new pattern bg

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import App from 'next/app'
 import Head from 'next/head'
+import './app.css'
 import bugsnag from '@bugsnag/js'
 import bugsnagReact from '@bugsnag/plugin-react'
 
@@ -11,7 +12,7 @@ const ErrorBoundary = bugsnagClient.getPlugin('react')
 class MyApp extends App {
   // Only uncomment this method if you have blocking data requirements for
   // every single page in your application. This disables the ability to
-  // perform automatic static optimization, causing every page in your app to
+      // perform automatic static optimization, causing every page in your app to
   // be server-side rendered.
   //
   // static async getInitialProps(appContext) {
@@ -32,10 +33,6 @@ class MyApp extends App {
         </Head>
         <Component {...pageProps} />
         <style jsx global>{`
-          html {
-            background-color: #ffffff;
-            background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%2378b85a' fill-opacity='0.28'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
-          }
           .content {
             margin: 80px 4em 0 4em;
             display: flex;
