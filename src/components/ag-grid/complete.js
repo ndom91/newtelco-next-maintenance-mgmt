8b643530a76cb8a71e6677e5d835bda94bc8cc69
ndo@ndo3 <yo@ndo.dev>
2020-04-21 11:19:17 +0200

    update: complete str > num

--- src/components/ag-grid/complete.js
+++ src/components/ag-grid/complete.js
@@ -7,14 +7,14 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 
 export default class CompleteIcon extends Component {
-  render () {
-    if (this.props.node.data.done === '1' || this.props.node.data.done === 'true') {
+  render() {
+    if (this.props.node.data.done === 1 || this.props.node.data.done === 'true') {
       return (
         <span style={{ display: 'flex', justifyContent: 'center', height: '50px', alignItems: 'center' }}>
           <FontAwesomeIcon style={{ fontSize: '12px' }} width='1.5em' icon={faCheckCircle} />
         </span>
       )
-    } else if (this.props.node.data.done === '0' || this.props.node.data.done === 'false') {
+    } else if (this.props.node.data.done === 0 || this.props.node.data.done === 'false') {
       return (
         <span style={{ display: 'flex', justifyContent: 'center', height: '50px', alignItems: 'center' }}>
           <FontAwesomeIcon style={{ fontSize: '12px' }} width='1.5em' icon={faTimes} />
