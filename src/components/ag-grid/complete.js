b1c9865581124e10041e7b13785b856edd22f60e
ndo@nt-batman <yo@ni.co.de>
2020-04-23 13:19:57 +0200

    update: ag-grid complete.js

--- src/components/ag-grid/complete.js
+++ src/components/ag-grid/complete.js
@@ -8,18 +8,20 @@ import {
 
 export default class CompleteIcon extends Component {
   render() {
-    if (this.props.node.data.done === 1 || this.props.node.data.done === 'true') {
-      return (
-        <span style={{ display: 'flex', justifyContent: 'center', height: '50px', alignItems: 'center' }}>
-          <FontAwesomeIcon style={{ fontSize: '12px' }} width='1.5em' icon={faCheckCircle} />
-        </span>
-      )
-    } else if (this.props.node.data.done === 0 || this.props.node.data.done === 'false') {
-      return (
-        <span style={{ display: 'flex', justifyContent: 'center', height: '50px', alignItems: 'center' }}>
-          <FontAwesomeIcon style={{ fontSize: '12px' }} width='1.5em' icon={faTimes} />
-        </span>
-      )
+    if (this.props.node.data.done) {
+      if (!!this.props.node.data.done) {
+        return (
+          <span style={{ display: 'flex', justifyContent: 'center', height: '50px', alignItems: 'center' }}>
+            <FontAwesomeIcon style={{ fontSize: '12px' }} width='1.5em' icon={faCheckCircle} />
+          </span>
+        )
+      } else (this.props.node.data.done === 0 || this.props.node.data.done === 'false') {
+        return (
+          <span style={{ display: 'flex', justifyContent: 'center', height: '50px', alignItems: 'center' }}>
+            <FontAwesomeIcon style={{ fontSize: '12px' }} width='1.5em' icon={faTimes} />
+          </span>
+        )
+      }
     } else {
       return (
         <span style={{ display: 'flex', justifyContent: 'center', height: '50px', alignItems: 'center' }}>
