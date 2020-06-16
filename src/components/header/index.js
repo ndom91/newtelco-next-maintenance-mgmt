55d4c3a19dede4559cf84eb531f40084ab79d72b
ndo@nt-batman <yo@ni.co.de>
2020-06-16 17:07:10 +0200

    add: kampsite link in dropdown menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -246,6 +246,26 @@ const MaintHeader = props => {
                 Settings
               </NavLink>
               <NavLink
+                icon={
+                  <svg
+                    height='16'
+                    width='16'
+                    fill='none'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    viewBox='0 0 24 24'
+                    stroke='currentColor'
+                  >
+                    <path d='M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4' />
+                  </svg>
+                }
+                href='https://newtelco.kampsite.co'
+                target='_blank'
+              >
+                Requests
+              </NavLink>
+              <NavLink
                 eventKey='2'
                 icon={
                   <svg
