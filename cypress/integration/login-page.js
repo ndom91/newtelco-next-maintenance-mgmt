e51cd98947226ed9d97ebc8cbf6af79ca92d62ef
ndo@$(hostname) <yo@ndo.dev>
2020-06-26 17:55:09 +0200

    update: cypress-social-login

--- cypress/integration/login-page.js
+++ cypress/integration/login-page.js
@@ -1,3 +1,11 @@
+import { GoogleSocialLogin } from 'cypress-social-logins'
+
+module.exports = (on, config) => {
+  on('task', {
+    GoogleSocialLogin: GoogleSocialLogin,
+  })
+}
+
 describe('Login page', () => {
   /*
    * Visits the page before each test
@@ -17,5 +25,46 @@ describe('Login page', () => {
       'href',
       'https://maint.newtelco.dev/api/auth/signin/google'
     )
+  })
+})
+
+describe('Login', () => {
+  it('Login through Google', () => {
+    const username = Cypress.env('device@newtelco.de')
+    const password = Cypress.env('N3wt3lco')
+    const loginUrl = Cypress.env('loginUrl')
+    const cookieName = Cypress.env('cookieName')
+    const socialLoginOptions = {
+      username,
+      password,
+      loginUrl,
+      headless: true,
+      logs: false,
+      loginSelector: '.signin-link',
+      postLoginSelector: '.rs-panel',
+    }
+
+    return cy
+      .task('GoogleSocialLogin', socialLoginOptions)
+      .then(({ cookies }) => {
+        cy.clearCookies()
+
+        const cookie = cookies
+          .filter(cookie => cookie.name === cookieName)
+          .pop()
+        if (cookie) {
+          cy.setCookie(cookie.name, cookie.value, {
+            domain: cookie.domain,
+            expiry: cookie.expires,
+            httpOnly: cookie.httpOnly,
+            path: cookie.path,
+            secure: cookie.secure,
+          })
+
+          Cypress.Cookies.defaults({
+            whitelist: cookieName,
+          })
+        }
+      })
   })
 })
