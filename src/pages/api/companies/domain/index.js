b842a40d6581b5b6ee999d3babb73a5c0637d456
ndom91 <yo@ndo.dev>
2020-09-22 10:39:51 +0200

    fix: allow multiple company domains

--- src/pages/api/companies/domain/index.js
+++ src/pages/api/companies/domain/index.js
@@ -2,13 +2,13 @@ const db = require('../../../../lib/db')
 const escape = require('sql-template-strings')
 
 module.exports = async (req, res) => {
-  const mailDomain = `${req.query.id.split('.')[0]}%`
+  const mailDomain = `%${req.query.id.split('.')[0]}%`
   const companyResults = await db.query(escape`
     SELECT 
     companies.id, companies.name 
     FROM companies 
     WHERE companies.mailDomain 
-    LIKE %${mailDomain}%
+    LIKE ${mailDomain}
   `)
   res.status(200).json({ companyResults })
 }
