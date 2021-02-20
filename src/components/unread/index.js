866f6ac7ae34c32c2d477f5f101444d2e6dda9bd
ndom91 <yo@ndo.dev>
2021-02-20 15:29:40 +0100

    update: home dashboard

--- src/components/unread/index.js
+++ src/components/unread/index.js
@@ -1,23 +1,23 @@
 import React, { useEffect } from 'react'
 import Link from 'next/link'
-import { Panel } from 'rsuite'
-import Store from '../store'
+import { Panel, Icon } from 'rsuite'
 import './unread.css'
 
-const UnreadBadge = ({ count }) => {
-  const store = Store.useStore()
-
-  useEffect(() => {
-    store.set('count')(count)
-  }, [count])
-
-  return (
-    <Panel header='Unread' bordered className='unread-panel'>
-      <Link href='/inbox' passHref>
-        <a className='unread-count'>{count}</a>
-      </Link>
-    </Panel>
-  )
-}
+const UnreadBadge = ({ count }) => (
+  <Panel
+    header={
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        Unread
+        <Icon icon='envelope-o' style={{ color: 'var(--primary)' }} size='lg' />
+      </div>
+    }
+    bordered
+    className='unread-panel'
+  >
+    <Link href='/inbox' passHref>
+      <a className='unread-count'>{count}</a>
+    </Link>
+  </Panel>
+)
 
 export default UnreadBadge
