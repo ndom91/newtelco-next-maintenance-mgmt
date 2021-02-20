80e1ebe29671349c01be13f4d585d899d24588f6
ndom91 <yo@ndo.dev>
2021-02-20 21:20:16 +0100

    fix: remove next-pwa

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const path = require('path')
 const webpack = require('webpack')
 const withCSS = require('@zeit/next-css')
 const withLess = require('@zeit/next-less')
-const withPWA = require('next-pwa')
+// const withPWA = require('next-pwa')
 const Dotenv = require('dotenv-webpack')
 require('dotenv').config()
 
@@ -50,126 +50,10 @@ const nextConfig = {
     modern: true,
   },
   maximumFileSizeToCacheInBytes: 5242880,
-  pwa: {
-    dest: 'public',
-    disable: dev,
-    runtimeCaching: [
-      {
-        // MUST be the same as "start_url" in manifest.json
-        urlPattern: '/',
-        // use NetworkFirst or NetworkOnly if you redirect un-authenticated user to login page
-        // use StaleWhileRevalidate if you want to prompt user to reload when new version available
-        handler: 'NetworkFirst',
-        options: {
-          // don't change cache name
-          cacheName: 'start-url',
-          expiration: {
-            maxEntries: 1,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
-      },
-      {
-        urlPattern: /api/i,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'next-api',
-          expiration: {
-            maxEntries: 16,
-            maxAgeSeconds: 60,
-          },
-        },
-      },
-      {
-        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-image-assets',
-          expiration: {
-            maxEntries: 64,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/api\.maintenance.newtelco.de\/.*/i,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'api',
-          expiration: {
-            maxEntries: 16,
-            maxAgeSeconds: 60,
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'google-fonts',
-          expiration: {
-            maxEntries: 4,
-            maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/use\.fontawesome\.com\/releases\/.*/i,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'font-awesome',
-          expiration: {
-            maxEntries: 1,
-            maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-          },
-        },
-      },
-      {
-        urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-font-assets',
-          expiration: {
-            maxEntries: 4,
-            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
-          },
-        },
-      },
-      {
-        urlPattern: /\.(?:js)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-js-assets',
-          expiration: {
-            maxEntries: 16,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
-      },
-      {
-        urlPattern: /\.(?:css|less)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-style-assets',
-          expiration: {
-            maxEntries: 16,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
-      },
-      {
-        urlPattern: /.*/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'others',
-          expiration: {
-            maxEntries: 16,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
-      },
-    ],
-  },
+  // pwa: {
+  //   dest: 'public',
+  //   disable: dev,
+  // },
   lessLoaderOptions: {
     javascriptEnabled: true,
   },
@@ -222,4 +106,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withBundleAnalyzer(withPWA(withLess(withCSS(nextConfig))))
+module.exports = withBundleAnalyzer(withLess(withCSS(nextConfig)))
