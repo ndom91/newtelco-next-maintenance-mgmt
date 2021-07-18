3ea7978bbd8aa95c2b152bb3159106459ea88fc5
ndom91 <yo@ndo.dev>
2021-07-18 16:56:03 +0200

    feat: remove rsuite dependencies

--- next.config.js
+++ next.config.js
@@ -1,55 +1,11 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
 const { withSentryConfig } = require('@sentry/nextjs')
-const withLess = require('@zeit/next-less')
-// Less Loader Workaround: https://github.com/vercel/next-plugins/issues/598#issuecomment-702907049
 const SentryWebpackPluginOptions = {
   silent: true,
 }
 
-function HACK_removeMinimizeOptionFromCssLoaders(config) {
-  // console.warn(
-  //   'HACK: Removing `minimize` option from `css-loader` entries in Webpack config'
-  // )
-  config.module.rules.forEach((rule) => {
-    if (Array.isArray(rule.use)) {
-      rule.use.forEach((u) => {
-        if (u.loader === 'css-loader' && u.options) {
-          delete u.options.minimize
-        }
-      })
-    }
-  })
-}
-
 const nextConfig = {
-  target: 'server',
-  compress: false,
-  productionBrowserSourceMaps: true, // For Sentry
-  experimental: {
-    modern: true,
-  },
   maximumFileSizeToCacheInBytes: 5242880,
-  lessLoaderOptions: {
-    javascriptEnabled: true,
-  },
-  webpack(config, options) {
-    HACK_removeMinimizeOptionFromCssLoaders(config)
-    config.stats = {
-      warningsFilter: (warn) => warn.indexOf('Conflicting order between:') > -1,
-    }
-    config.plugins.push(
-      new options.webpack.DefinePlugin({
-        'process.env.NEXT_IS_SERVER': JSON.stringify(
-          options.isServer.toString()
-        ),
-      })
-    )
-  },
+  cssModules: true,
 }
 
-module.exports = withSentryConfig(
-  withBundleAnalyzer(withLess(nextConfig)),
-  SentryWebpackPluginOptions
-)
+module.exports = withSentryConfig(nextConfig, SentryWebpackPluginOptions)
