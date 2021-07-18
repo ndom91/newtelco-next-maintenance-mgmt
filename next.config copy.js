962581631cd608c925dc48b48441ba7acdb3bdae
ndom91 <yo@ndo.dev>
2021-07-18 16:25:17 +0200

    fix: hooks

--- next.config copy.js
+++ next.config copy.js
@@ -2,7 +2,22 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 const withLess = require('@zeit/next-less')
+
 // Less Loader Workaround: https://github.com/vercel/next-plugins/issues/598#issuecomment-702907049
+
+const SentryWebpackPlugin = require('@sentry/webpack-plugin')
+const {
+  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  NODE_ENV,
+  VERCEL_GITHUB_COMMIT_SHA,
+} = process.env
+
+const COMMIT_SHA = VERCEL_GITHUB_COMMIT_SHA
+
+process.env.SENTRY_DSN = SENTRY_DSN
 
 function HACK_removeMinimizeOptionFromCssLoaders(config) {
   // console.warn(
@@ -30,10 +45,16 @@ const nextConfig = {
   lessLoaderOptions: {
     javascriptEnabled: true,
   },
+  env: {
+    NEXT_PUBLIC_COMMIT_SHA: COMMIT_SHA,
+  },
   webpack(config, options) {
     HACK_removeMinimizeOptionFromCssLoaders(config)
     config.stats = {
       warningsFilter: (warn) => warn.indexOf('Conflicting order between:') > -1,
+    }
+    if (!options.isServer) {
+      config.resolve.alias['@sentry/node'] = '@sentry/browser'
     }
     config.plugins.push(
       new options.webpack.DefinePlugin({
@@ -42,6 +63,27 @@ const nextConfig = {
         ),
       })
     )
+    if (
+      SENTRY_DSN &&
+      SENTRY_ORG &&
+      SENTRY_PROJECT &&
+      SENTRY_AUTH_TOKEN &&
+      COMMIT_SHA &&
+      NODE_ENV === 'production'
+    ) {
+      config.plugins.push(
+        new SentryWebpackPlugin({
+          include: '.next',
+          ignore: ['node_modules'],
+          stripPrefix: ['webpack://_N_E/'],
+          urlPrefix: '~/_next',
+          release: COMMIT_SHA,
+          org: 'newtelco-gmbh',
+          project: 'next-maintenance',
+        })
+      )
+    }
+    return config
   },
 }
 
