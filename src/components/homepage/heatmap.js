35f3c80213f334da3657de9a75a69ae0bd0a3d2f
Nico Domino <yo@ndo.dev>
2022-04-15 15:23:10 +0000

    fix: dynamic heatmap dates for year - 1

--- src/components/homepage/heatmap.js
+++ src/components/homepage/heatmap.js
@@ -30,8 +30,8 @@ const Heatmap = () => {
             data={data.maintenances}
             minValue={0}
             maxValue={10}
-            from={"2020-01-01"}
-            to={"2021-01-01"}
+            from={format(startOfYear(new Date()), "yyyy-LL-dd")}
+            to={format(subYears(startOfYear(new Date()), 1), "yyyy-LL-dd")}
             emptyColor="#eeeeee"
             colors={["#C6E2BA", "#8DC574", "#67B246", "#5A9C3D"]}
             margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
